9496d4b08a862298d8de9a3d9e143db9811d8ee6
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-04 12:14:41 -0500

    add persons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> personsList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          personsList.add({
+            "name": "Jhonny",
+            "institution": "TECSUP",
+            "direccion": "av 123 peru",
+          });
+          setState(() {});
+        },
+      ),
+      appBar: AppBar(
+        title: Text("Matriculas App"),
+      ),
+      body: Column(
+        children: [
+          Text("Las personas de mi aplicativo son;"),
+          ...personsList.map((element) {
+            return ListTile(
+              title: Text(element["name"]),
+              subtitle: Text(element["institution"]),
+              trailing: Icon(Icons.add),
+              leading: Icon(Icons.add),
+            );
+          }).toList()
+        ],
+      ),
+    );
   }
 }
