9d484f15460a18684c9da312329ab7e645472bca
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-09 19:40:19 -0500

    clear data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,49 +30,61 @@ class _HomePageState extends State<HomePage> {
             },
             icon: Icon(Icons.add),
           ),
+          IconButton(
+            onPressed: () {
+              institucionesList.clear();
+              setState(() {});
+            },
+            icon: Icon(Icons.delete),
+          ),
         ],
       ),
-      body: Column(
-        children: [
-          ...institucionesList.map((institution) {
-            return Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      institution.nombre,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          children: [
+            ...institucionesList.map((institution) {
+              return Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        institution.nombre,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        institution.matriculas.add(matricula1);
-                        setState(() {});
-                      },
-                      icon: Icon(Icons.add),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                ...institution.matriculas.map((matricula) {
-                  return ListTile(
-                    title: Text(matricula.persona.name),
-                    subtitle: Text(matricula.carrera.nombre),
-                    trailing: Icon(Icons.edit),
-                    leading: Icon(Icons.person),
-                  );
-                }),
-              ],
-            );
-          }).toList(),
-          Text("Hecho por el grupo g9")
-        ],
+                      IconButton(
+                        onPressed: () {
+                          institution.matriculas.add(matricula1);
+                          setState(() {});
+                        },
+                        icon: Icon(Icons.add),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          institution.matriculas.clear();
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  ...institution.matriculas.map((matricula) {
+                    return ListTile(
+                      title: Text(matricula.persona.name),
+                      subtitle: Text(matricula.carrera.nombre),
+                      trailing: Icon(Icons.edit),
+                      leading: Icon(Icons.person),
+                    );
+                  }),
+                ],
+              );
+            }).toList(),
+            Text("Hecho por el grupo g9")
+          ],
+        ),
       ),
     );
   }
